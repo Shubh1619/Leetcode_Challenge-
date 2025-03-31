7b92983b509909830026f4f7603dc9586a62f92b
Shubh1619 <ubalekartik203@gmail.com>
2025-03-31 12:36:05 +0530

    1st problem

---
+++ _1to30/1.py
@@ -0,0 +1,20 @@
+# Problem 1: Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+nums = [2,7,11,15]
+target = 9
+def twoSum(nums, target):
+    num_map = {}  
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))  
+
+
